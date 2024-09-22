c08d1aa19079e550fce6ecaf65609d4e7c6525fe
Gioh0 <125033740+Gioh0@users.noreply.github.com>
2024-09-22 00:25:35 +0000

    adicionando fotos

--- src/app/shared/components/treinos/treinos.component.ts
+++ src/app/shared/components/treinos/treinos.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class TreinosComponent  implements OnInit {
 
   treinos = {
+    braco: [
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/03/rosca-biceps-com-pegada-fechada-na-barra-tradicional.gif'},
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2019/11/03081301-crucifixo-com-halteres.gif'},
+   ],
     peito: [
       {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-reto-com-halteres.gif'},
       {exercicio: 'https://www.hipertrofia.org/blog/wp-content/uploads/2020/06/dumbbell-reverse-bench-press.gif'}, 
+    ],
+    costas: [
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/costas-remada-unilateral-com-halter-serrote-no-banco.gif'},
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/costas-remada-curvada-.gif'},
+    ],
+    perna: [
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/06/agachamento-com-salto-pernas-e-panturrilhas.gif'},
+      {exercicio: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/pernas-afundo-tradicional-sem-pesos.gif'},
     ]
   }
 
