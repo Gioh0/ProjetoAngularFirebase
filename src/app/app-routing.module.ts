05bfa31c64f078d99eea039c02663d7b2d5830d2
Gioh0 <125033740+Gioh0@users.noreply.github.com>
2024-11-09 01:48:26 +0000

    criação de puma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'feedback',
+    loadChildren: () => import('./feedback/feedback.module').then( m => m.FeedbackPageModule)
   }
 ];
 
--- src/app/feedback/feedback.module.ts
+++ src/app/feedback/feedback.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { IonicModule } from '@ionic/angular';
+
+import { FeedbackPageRoutingModule } from './feedback-routing.module';
+
+import { FeedbackPage } from './feedback.page';
 import { MenuModule } from '../shared/components/menu/menu.module';
 import { FooterModule } from '../shared/components/footer/footer.module';
-
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule,
+    FeedbackPageRoutingModule,
     MenuModule,
     FooterModule
   ],
-  declarations: [HomePage]
+  declarations: [FeedbackPage]
 })
-export class HomePageModule {}
+export class FeedbackPageModule {}
